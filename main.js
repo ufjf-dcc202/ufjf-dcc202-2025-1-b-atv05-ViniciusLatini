acaf015b3269defc93446d231cfef6e2a597221e
Vinicius Latini <vinicius.latini@estudante.ufjf.br>
2025-07-30 16:48:09 -0300

    adiciona cria discos

--- main.js
+++ main.js
@@ -4,6 +4,17 @@ eH1.textContent = "Ola mundo"
 const eTabuleiro = criaTabuleiro();
 document.body.appendChild(eTabuleiro);
 
+for(let i = 0; i < 7; i++) {
+  const eDisco = criaDisco();
+  eTabuleiro.appendChild(eDisco);
+}
+
+function criaDisco() {
+  const novoDisco = document.createElement('div');
+  novoDisco.classList.add('disco');
+  return novoDisco;
+}
+
 function criaTabuleiro() {
   const novoTabuleiro = document.createElement('div');
   novoTabuleiro.classList.add('tabuleiro');
