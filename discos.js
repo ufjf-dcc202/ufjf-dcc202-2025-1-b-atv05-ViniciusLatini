f53d400d3090d638a51e546c323eb885ed1c9100
Vinicius Latini <vinicius.latini@estudante.ufjf.br>
2025-07-30 17:53:29 -0300

    corrige condição de movimento no jogo de discos

--- discos.js
+++ discos.js
@@ -28,7 +28,7 @@ function mover(origem, destino) {
   if (tabuleiro[destino] !== "transparente") return;
   if (Math.abs(destino - origem) > 2) return;
   if (destino - origem === 2 && tabuleiro[origem + 1] === "transparente") return;
-  if (origem - destino === 2 && tabuleiro[origem - 1] === "transparente") return;
+  if (destino - origem === -2 && tabuleiro[origem - 1] === "transparente") return;
 
   tabuleiro[destino] = tabuleiro[origem];
   tabuleiro[origem] = "transparente";
