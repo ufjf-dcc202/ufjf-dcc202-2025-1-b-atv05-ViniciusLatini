765cd51e0aec56bde1243261ca72511a144aade7
Vinicius Latini <vinicius.latini@estudante.ufjf.br>
2025-07-30 16:36:12 -0300

    adiciona e cria o tabuleiro

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 const eH1 = document.querySelector('h1');
 eH1.textContent = "Ola mundo"
+
+const eTabuleiro = criaTabuleiro();
+document.body.appendChild(eTabuleiro);
+
+function criaTabuleiro() {
+  const novoTabuleiro = document.createElement('div');
+  novoTabuleiro.classList.add('tabuleiro');
+  return novoTabuleiro;
+}
